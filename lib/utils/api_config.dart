46df7af160160eaa6c4739899008e1f1a61b3b8a
abdalrhamnmohamed4 <abdalrhamnmohamed4@gmail.com>
2026-04-14 23:03:02 +0200

    ubdate

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Development
-  static const String baseUrl = 'http://10.0.2.2:8000'; // Android Emulator
+  static const String baseUrl = 'http://51.21.2.0:8000';
   // static const String baseUrl = 'http://localhost:8000'; // iOS Simulator
   // static const String baseUrl = 'https://api.yallatrip.com'; // Production
 }
